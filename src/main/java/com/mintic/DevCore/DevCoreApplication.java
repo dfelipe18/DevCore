abac24fd91f20588117dd276a1202cddf7b6963f
Felipe Diaz <andresfelipejaimesdiaz@gmail.com>
2022-08-22 13:06:11 -0500

    First commit

---
+++ src/main/java/com/mintic/DevCore/DevCoreApplication.java
@@ -0,0 +1,13 @@
+package com.mintic.DevCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCoreApplication.class, args);
+	}
+
+}
