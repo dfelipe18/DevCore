1d69a7e63b7546cad3d8eba52f7a2a6a2d8fddf7
dasdo <tuemail@ejemplo.com>
2022-08-24 17:41:31 -0500

    Entidad Empleado

--- src/main/java/com/mintic/model/Empleado.java
+++ src/main/java/com/mintic/model/Empleado.java
@@ -1,4 +1,50 @@
 package com.mintic.model;
 
 public class Empleado {
+    
+    private String nombre;
+    private String correo;
+    private String empresa;
+    private String rol;
+
+    public Empleado(String nombre, String correo, String empresa, String rol) {
+        this.nombre = nombre;
+        this.correo = correo;
+        this.empresa = empresa;
+        this.rol = rol;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+    
 }
