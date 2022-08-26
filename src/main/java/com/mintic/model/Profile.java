70c93a102f0a62ec269bf145cf6d8a6f367cbaa9
Felipe Diaz <andresfelipejaimesdiaz@gmail.com>
2022-08-25 19:07:29 -0500

    Add structure of class Profile

--- src/main/java/com/mintic/model/Profile.java
+++ src/main/java/com/mintic/model/Profile.java
@@ -1,9 +1,72 @@
 package com.mintic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "Profile")
 public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String image;
+    private String phone;
+    @CreatedDate
+    private Date createdAt;
+    @LastModifiedDate
+    private Date updateAt;
+
+    public Profile(){}
+
+    public Profile(String id, String image, String phone, Date createdAt, Date updateAt) {
+        this.id = id;
+        this.image = image;
+        this.phone = phone;
+        this.createdAt = createdAt;
+        this.updateAt = updateAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
 }
