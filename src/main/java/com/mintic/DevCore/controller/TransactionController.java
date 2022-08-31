e0c45185dfb66729be86ed8f475845d62d831a19
Felipe Diaz <andresfelipejaimesdiaz@gmail.com>
2022-08-31 16:42:22 -0500

    Instance of notation @Data

--- src/main/java/com/mintic/DevCore/controller/TransactionController.java
+++ src/main/java/com/mintic/DevCore/controller/TransactionController.java
@@ -1,4 +1,9 @@
 package com.mintic.DevCore.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/services")
 public class TransactionController {
 }
--- src/main/java/com/mintic/DevCore/model/Transaction.java
+++ src/main/java/com/mintic/DevCore/model/Transaction.java
@@ -1,11 +1,13 @@
 package com.mintic.DevCore.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
 @Entity
 @Table(name = "transaction")
 public class Transaction {
@@ -26,72 +28,4 @@ public class Transaction {
     @LastModifiedDate
     private Date updateAt;
 
-    public Transaction() {
-    }
-
-    public Transaction(long id, String concept, float amount, Employee users, Enterprise enterprise, Date createdAt, Date updateAt) {
-        this.id = id;
-        this.concept = concept;
-        this.amount = amount;
-        this.users = users;
-        this.enterprise = enterprise;
-        this.createdAt = createdAt;
-        this.updateAt = updateAt;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getConcept() {
-        return concept;
-    }
-
-    public void setConcept(String concept) {
-        this.concept = concept;
-    }
-
-    public float getAmount() {
-        return amount;
-    }
-
-    public void setAmount(float amount) {
-        this.amount = amount;
-    }
-
-    public Employee getUsers() {
-        return users;
-    }
-
-    public void setUsers(Employee users) {
-        this.users = users;
-    }
-
-    public Enterprise getEnterprise() {
-        return enterprise;
-    }
-
-    public void setEnterprise(Enterprise enterprise) {
-        this.enterprise = enterprise;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdateAt() {
-        return updateAt;
-    }
-
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
-    }
 }
