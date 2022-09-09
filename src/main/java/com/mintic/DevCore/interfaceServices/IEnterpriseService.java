f4721329c42de1dca88473e63e539a7512fbf5c4
Filleral Mahecha <filleral19@gmail.com>
2022-09-08 20:43:02 -0500

    Actualizacion de Enterprise

--- src/main/java/com/mintic/DevCore/interfaceServices/IEnterpriseService.java
+++ src/main/java/com/mintic/DevCore/interfaceServices/IEnterpriseService.java
@@ -1,4 +1,27 @@
 package com.mintic.DevCore.interfaceServices;
 
+import com.mintic.DevCore.model.Enterprise;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
 public interface IEnterpriseService {
+
+    @GetMapping("/get-all-enterprise")
+    List<Enterprise> getAllEnterprises();
+
+    @GetMapping("/get-enterprise/{id}")
+    ResponseEntity<Enterprise> getEnterpriseById(@PathVariable Long id);
+
+    @PostMapping("/save-enterprise")
+    ResponseEntity<Enterprise> createEnterprise(@RequestBody Enterprise transaction);
+
+    @PatchMapping("/update-enterprise/{id}")
+    ResponseEntity<Enterprise> updateEnterprise(@PathVariable Long id, @RequestBody Map<Object, Object> fields);
+
+    @DeleteMapping("/delete-enterprise/{id}")
+    ResponseEntity<Void> deleteEnterprise(@PathVariable("id") Long id);
+
 }
