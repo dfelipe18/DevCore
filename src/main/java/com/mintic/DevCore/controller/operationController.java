3ff2b88f6b6f64a51fbeecbf6bb685827556398b
Felipe Diaz <andresfelipejaimesdiaz@gmail.com>
2022-09-28 19:25:01 -0500

    add new dependency

---
+++ src/main/java/com/mintic/DevCore/controller/operationController.java
@@ -0,0 +1,32 @@
+package com.mintic.DevCore.controller;
+
+import com.mintic.DevCore.interfaces.IEmployee;
+import com.mintic.DevCore.interfaces.IEnterprise;
+import com.mintic.DevCore.interfaces.ITransaction;
+import com.mintic.DevCore.model.Enterprise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class operationController {
+
+    @Autowired
+    private IEnterprise enterpriseRepo;
+
+    @Autowired
+    private IEmployee employeeRepo;
+
+    @Autowired
+    private ITransaction transactionRepo;
+
+    @GetMapping("/enterprises")
+    public String getEnterprise(Model model) {
+        List <Enterprise> enterprises = enterpriseRepo.findAll();
+        model.addAttribute("enterprises", enterprises);
+        return "enterprises";
+    }
+}
