2d4a13a89705708bf0b892e6c700e3fc52d9dccf
Filleral Mahecha <filleral19@gmail.com>
2022-09-28 23:19:50 -0500

    adios vaquero

--- src/main/java/com/mintic/DevCore/controller/operationEmployeeController.java
+++ src/main/java/com/mintic/DevCore/controller/operationEmployeeController.java
@@ -3,11 +3,14 @@ package com.mintic.DevCore.controller;
 import com.mintic.DevCore.model.Employee;
 import com.mintic.DevCore.model.Enterprise;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-
+@Controller
 public class operationEmployeeController {
 
     @Autowired
@@ -20,7 +23,19 @@ public class operationEmployeeController {
         return "get-employee";
     }
     @GetMapping("/create-employee")
-    public String createEnterprises() {
+    public String createEmployee(Model model) {
+        model.addAttribute("employee", new Employee());
         return "create-employee";
+    }
+
+    @PostMapping("/save-employee")
+    public String saveEmployee(Employee employee) {
+        ResponseEntity<Employee> response = employeesRepo.saveEmployee(employee);
+        if (response.getStatusCodeValue() == 200) {
+            return "redirect:/get-employee";
+        } else {
+            return "create-employee";
+        }
+
     }
 }
